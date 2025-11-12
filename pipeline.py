d4ec9809028a3444e70f161f3f359b166b81d258
Bruno <bruno_casagrande@outlook.com.br>
2025-11-12 13:47:00 -0300

    saving file in dif folder

--- pipeline.py
+++ pipeline.py
@@ -1,8 +1,19 @@
 import kagglehub
+import shutil
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-# Download latest version
-path = kagglehub.dataset_download("nome/projeto/pipeline/data/raw")
+path = kagglehub.dataset_download("rtatman/chocolate-bar-ratings")
 
-print("Path to dataset files:", path)
+raw_data_folder = "data/raw"
 
-# teste
+shutil.copytree(path, raw_data_folder, dirs_exist_ok=True)
+
+print("Caminho do arquivo dataset:", path)
+print("Copiado para: ", os.path.abspath(raw_data_folder))
+
+data = pd.read_csv('C:/Users/Bruno/Desktop/nome/projeto/pipeline/data/raw/flavors_of_cacao.csv')
+print(data.shape)
